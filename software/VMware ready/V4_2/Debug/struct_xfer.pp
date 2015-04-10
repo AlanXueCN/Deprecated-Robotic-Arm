22ece3817918f3b1f47d316479e2138fe8f80ee8
Drue Satterfield <drscp5@gmail.com>
2015-04-09 21:22:58 -0500

    Updated for VMWare. Cleaned organization up
    Updated for VMWare. Cleaned organization up

---
+++ software/VMware ready/V4_2/Debug/struct_xfer.pp
@@ -0,0 +1,27 @@
+# FIXED
+
+struct_xfer.obj: ../struct_xfer.c
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h
+struct_xfer.obj: ../struct_xfer.h
+struct_xfer.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h
+struct_xfer.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+struct_xfer.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/UART.h
+struct_xfer.obj: ../structs.h
+
+../struct_xfer.c: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h: 
+../struct_xfer.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/UART.h: 
+../structs.h: 
