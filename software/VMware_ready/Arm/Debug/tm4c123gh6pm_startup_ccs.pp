10a9bedcbfedf77b93f53ca55b12ed2e6de3f540
Drue Satterfield <drscp5@mst.edu>
2015-04-17 20:47:57 -0500

    attempting to try out this whole git shell thing. Gah.

---
+++ software/VMware_ready/Arm/Debug/tm4c123gh6pm_startup_ccs.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h
+tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+
+../tm4c123gh6pm_startup_ccs.c: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h: 
+/opt/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
